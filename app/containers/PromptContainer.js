2c9298d61f1c9f27d63be580d7d2e6c47e960936
Marvki <marvkipi@gmail.com>
2016-06-21 22:51:57 -0700

    playerone ui

---
+++ app/containers/PromptContainer.js
@@ -0,0 +1,30 @@
+var React = require('react');
+
+var PromptContainer = React.createClass({
+  render: function() {
+    return (
+      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center">
+        <h1>Player 1</h1>
+        <div className="col-md-12">
+          <form>
+            <div className="form-group">
+              <input
+                className="form-control"
+                placeholder="Gihub username"
+                type="text" />
+            </div>
+            <div className="form-group col-sm-4 col-sm-offset-4">
+              <button
+                className="btn btn-block btn-success"
+                type="submit">
+                Continue
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    )
+  }
+});
+
+module.exports = PromptContainer;
